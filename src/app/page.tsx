3f5d923e6609b2fbf2e3842fa5d16b749043d4bb
MostakemHossain <mostakememon123@gmail.com>
2024-05-19 19:32:40 +0600

    Initial project setup and mui intregrate

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Button from "@mui/material/Button";
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home page</h1>
+      <Button>Text</Button>
+    </div>
+  );
+};
+
+export default HomePage;
