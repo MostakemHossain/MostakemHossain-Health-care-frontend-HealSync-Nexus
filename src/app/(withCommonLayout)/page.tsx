706c99d828a1ee27c943f0583ea4f86a92467063
MostakemHossain <mostakememon123@gmail.com>
2024-05-20 21:02:28 +0600

    Top rated doctors

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/UI/HomePage/HeroSection/HeroSection";
 import Specialist from "@/components/UI/HomePage/Specialist/Specialist";
+import TopRatedDoctors from "@/components/UI/HomePage/TopRatedDoctors/TopRatedDoctors";
 const HomePage = () => {
   return (
     <>
       <HeroSection />
       <Specialist />
+      <TopRatedDoctors />
     </>
   );
 };
