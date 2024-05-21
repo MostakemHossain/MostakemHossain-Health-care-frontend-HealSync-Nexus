67c29ce234df221e5488f3e4f9032a3c7b20b763
MostakemHossain <mostakememon123@gmail.com>
2024-05-21 10:26:21 +0600

    patient register page added

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,8 +1,141 @@
+import assets from "@/assets";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
+
 const Register = () => {
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
+    <Container
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <Stack
+        sx={{
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          maxWidth: 600,
+        }}
+      >
+        <Box
+          sx={{
+            width: "100%",
+            boxShadow: 1,
+            borderRadius: 1,
+            p: 4,
+            textAlign: "center",
+          }}
+        >
+          <Stack
+            sx={{
+              justifyContent: "center",
+              alignItems: "center",
+              mb: 3,
+            }}
+          >
+            <Box>
+              <Image src={assets.svgs.logo} width={50} height={50} alt="logo" />
+            </Box>
+            <Box>
+              <Typography variant="h6" fontWeight={600}>
+                Patient Register
+              </Typography>
+            </Box>
+          </Stack>
+          <form>
+            <Box>
+              <Grid container spacing={2} my={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    id="outlined-basic"
+                    label="Name"
+                    type="text"
+                    variant="outlined"
+                    size="small"
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    id="outlined-basic"
+                    label="Email"
+                    type="email"
+                    variant="outlined"
+                    size="small"
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    id="outlined-basic"
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                    size="small"
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    id="outlined-basic"
+                    label="Contact Number"
+                    type="text"
+                    variant="outlined"
+                    size="small"
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    id="outlined-basic"
+                    label="Address"
+                    type="text"
+                    variant="outlined"
+                    size="small"
+                    fullWidth
+                  />
+                </Grid>
+              </Grid>
+              <Button
+                sx={{
+                  margin: "10px 0px",
+                }}
+                fullWidth
+              >
+                Register
+              </Button>
+              <Typography component="p" fontWeight={400}>
+                Do you already have an account?{" "}
+                <Link href="/login" passHref>
+                  <Typography
+                    component="a"
+                    sx={{
+                      color: "blue",
+                      textDecoration: "underline",
+                      cursor: "pointer",
+                    }}
+                  >
+                    Login
+                  </Typography>
+                </Link>
+              </Typography>
+            </Box>
+          </form>
+        </Box>
+      </Stack>
+    </Container>
   );
 };
 
--- src/components/UI/HomePage/HowItWorks/WhoItWorks.tsx
+++ src/components/UI/HomePage/HowItWorks/WhoItWorks.tsx
@@ -30,7 +30,7 @@ const HowItWorks = () => {
             fontWeight={400}
             sx={{ mt: 2 }}
           >
-            Access to expert physicians and surgeons, advanced technologies
+            Access to expert physicians and surgeons, advanced technologies,
           </Typography>
           <Typography component="p" fontSize={18} fontWeight={400}>
             and top-quality medical care from the comfort of your home.
@@ -38,127 +38,76 @@ const HowItWorks = () => {
         </Box>
         <Box>
           <Grid container spacing={2} mt={5}>
-            <Grid item xs={6}>
+            <Grid item xs={12} md={6}>
               <Image src={femaleDocImg} alt="doctor image" />
             </Grid>
-            <Grid item xs={6}>
+            <Grid item xs={12} md={6}>
               <Grid container spacing={2}>
-                <Grid item xs={6}>
-                  <Box
-                    sx={{
-                      backgroundColor: "#fff",
-                      border: "1px solid lightgray",
-                      borderRadius: "10px",
-                      padding: "20px",
-                    }}
-                  >
-                    <Image src={searchIcon} alt="search-icon" />
-                    <Typography
-                      variant="h6"
-                      component="h2"
-                      fontWeight={500}
-                      mt={3}
+                {[
+                  {
+                    icon: searchIcon,
+                    title: "Search for a Doctor",
+                    description:
+                      "Find experienced doctors from our comprehensive database based on your health needs.",
+                  },
+                  {
+                    icon: doctorIcon,
+                    title: "Check Doctor Profiles",
+                    description:
+                      "Review qualifications, specialties, and patient feedback to choose the best doctor for you.",
+                  },
+                  {
+                    icon: appointmentIcon,
+                    title: "Schedule an Appointment",
+                    description:
+                      "Easily book an appointment at a time that suits your schedule.",
+                  },
+                  {
+                    icon: charityIcon,
+                    title: "Receive Your Treatment",
+                    description:
+                      "Get the medical advice and treatment you need through a video consultation.",
+                  },
+                ].map((step, index) => (
+                  <Grid item xs={12} sm={6} key={index}>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "space-between",
+                        backgroundColor: "#fff",
+                        border: "1px solid lightgray",
+                        borderRadius: "10px",
+                        padding: "20px",
+                        height: "100%", // Ensuring equal height for all cards
+                      }}
                     >
-                      Search for a Doctor
-                    </Typography>
-                    <Typography
-                      component="p"
-                      fontSize={14}
-                      fontWeight={400}
-                      sx={{ mt: 1 }}
-                    >
-                      Find experienced doctors from our comprehensive database
-                      based on your health needs.
-                    </Typography>
-                  </Box>
-                </Grid>
-                <Grid item xs={6}>
-                  <Box
-                    sx={{
-                      backgroundColor: "#fff",
-                      border: "1px solid lightgray",
-                      borderRadius: "10px",
-                      padding: "20px",
-                    }}
-                  >
-                    <Image src={doctorIcon} alt="doctor-icon" />
-                    <Typography
-                      variant="h6"
-                      component="h2"
-                      fontWeight={500}
-                      mt={3}
-                    >
-                      Check Doctor Profiles
-                    </Typography>
-                    <Typography
-                      component="p"
-                      fontSize={14}
-                      fontWeight={400}
-                      sx={{ mt: 1 }}
-                    >
-                      Review qualifications, specialties, and patient feedback
-                      to choose the best doctor for you.
-                    </Typography>
-                  </Box>
-                </Grid>
-                <Grid item xs={6}>
-                  <Box
-                    sx={{
-                      backgroundColor: "#fff",
-                      border: "1px solid lightgray",
-                      borderRadius: "10px",
-                      padding: "20px",
-                    }}
-                  >
-                    <Image src={appointmentIcon} alt="appointment-icon" />
-                    <Typography
-                      variant="h6"
-                      component="h2"
-                      fontWeight={500}
-                      mt={3}
-                    >
-                      Schedule an Appointment
-                    </Typography>
-                    <Typography
-                      component="p"
-                      fontSize={14}
-                      fontWeight={400}
-                      sx={{ mt: 1 }}
-                    >
-                      Easily book an appointment at a time that suits your
-                      schedule.
-                    </Typography>
-                  </Box>
-                </Grid>
-                <Grid item xs={6}>
-                  <Box
-                    sx={{
-                      backgroundColor: "#fff",
-                      border: "1px solid lightgray",
-                      borderRadius: "10px",
-                      padding: "20px",
-                    }}
-                  >
-                    <Image src={charityIcon} alt="solution-icon" />
-                    <Typography
-                      variant="h6"
-                      component="h2"
-                      fontWeight={500}
-                      mt={3}
-                    >
-                      Receive Your Treatment
-                    </Typography>
-                    <Typography
-                      component="p"
-                      fontSize={14}
-                      fontWeight={400}
-                      sx={{ mt: 1 }}
-                    >
-                      Get the medical advice and treatment you need through a
-                      video consultation.
-                    </Typography>
-                  </Box>
-                </Grid>
+                      <Image
+                        src={step.icon}
+                        alt={`${step.title.toLowerCase()}-icon`}
+                      />
+                      <Typography
+                        variant="h6"
+                        component="h2"
+                        fontWeight={500}
+                        mt={3}
+                        textAlign="center"
+                      >
+                        {step.title}
+                      </Typography>
+                      <Typography
+                        component="p"
+                        fontSize={14}
+                        fontWeight={400}
+                        mt={1}
+                        textAlign="center"
+                      >
+                        {step.description}
+                      </Typography>
+                    </Box>
+                  </Grid>
+                ))}
               </Grid>
             </Grid>
           </Grid>
