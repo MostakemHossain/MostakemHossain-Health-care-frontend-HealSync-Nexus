675862461ae541195750c21ba2fbc26cf21d648e
MostakemHossain <mostakememon123@gmail.com>
2024-05-22 13:03:42 +0600

    active link set

--- src/components/DashBoard/Sidebar/Sidebar.tsx
+++ src/components/DashBoard/Sidebar/Sidebar.tsx
@@ -1,33 +1,12 @@
 import { userRole } from "@/types";
 import { drawerItems } from "@/utils/drawerItems";
-import MailIcon from "@mui/icons-material/Mail";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
 import { Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
 import Link from "next/link";
+import SidebarItem from "./SidebarItem";
 
 const Sidebar = () => {
-  const drawer = (
-    <div>
-      <List>
-        {drawerItems("admin" as userRole).map((item, index) => (
-          <ListItem key={index} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={item.title} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
   return (
     <Box>
       <Typography variant="h5" p={2} component={Link} href="/" fontWeight={600}>
@@ -37,7 +16,11 @@ const Sidebar = () => {
         </Box>{" "}
         Nexus
       </Typography>
-      {drawer}
+      <List>
+        {drawerItems("admin" as userRole).map((item, index) => (
+          <SidebarItem key={index} item={item} />
+        ))}
+      </List>
     </Box>
   );
 };
---
+++ src/components/DashBoard/Sidebar/SidebarItem.tsx
@@ -0,0 +1,42 @@
+import { DrawerItem } from "@/types";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type IProps = {
+  item: DrawerItem;
+};
+
+const SidebarItem = ({ item }: IProps) => {
+  const linkPath = `/dashboard/${item.path}`;
+  const pathName = usePathname();
+
+  return (
+    <Link href={linkPath}>
+      <ListItem
+        disablePadding
+        sx={{
+          ...(pathName === linkPath
+            ? {
+                borderRight: "3px solid #1586FD",
+                "& svg": {
+                  color: "#1586FD",
+                },
+              }
+            : {}),
+          mb: 1,
+        }}
+      >
+        <ListItemButton>
+          <ListItemIcon>{item.icon && <item.icon />}</ListItemIcon>
+          <ListItemText primary={item.title} />
+        </ListItemButton>
+      </ListItem>
+    </Link>
+  );
+};
+
+export default SidebarItem;
